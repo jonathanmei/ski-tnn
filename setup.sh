b0e19c6c1f4de6cb6c8dfb9ebd1dc47325e1d90e
Jonathan Mei <jmei@visby.io>
2023-05-21 14:15:11 -0700

    pin official fairseq; wiki preprocessing works

--- setup.sh
+++ setup.sh
@@ -12,35 +12,7 @@ source ~/.bashrc
 
 #create envs
 conda env create --file tnn.yaml
-conda activate tnn
-cd fairseq-tnn
-pip install --editable ./
-cd ..
 conda env create --file lra.yaml
 # autoregressive LM
 # preprocess data
-wget https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-raw-v1.zip
-unzip wikitext-103-raw-v1.zip
-# encode with BPE
-mkdir -p gpt2_bpe
-wget -O gpt2_bpe/encoder.json https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/encoder.json
-wget -O gpt2_bpe/vocab.bpe https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/vocab.bpe
-for SPLIT in train valid test; do \
-    python -m examples.roberta.multiprocessing_bpe_encoder \
-        --encoder-json gpt2_bpe/encoder.json \
-        --vocab-bpe gpt2_bpe/vocab.bpe \
-        --inputs wikitext-103-raw/wiki.${SPLIT}.raw \
-        --outputs wikitext-103-raw/wiki.${SPLIT}.bpe \
-        --keep-empty \
-        --workers 60; \
-done
-# preproc/binarize data using GPT-2 fairseq dictionary
-wget -O gpt2_bpe/dict.txt https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/dict.txt
-fairseq-preprocess \
-    --only-source \
-    --srcdict gpt2_bpe/dict.txt \
-    --trainpref wikitext-103-raw/wiki.train.bpe \
-    --validpref wikitext-103-raw/wiki.valid.bpe \
-    --testpref wikitext-103-raw/wiki.test.bpe \
-    --destdir data-bin/wikitext-103 \
-    --workers 60
+source setup_data.sh
